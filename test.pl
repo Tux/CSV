80c8312cc1c7901a07e1d690bb17cdb0dfda60dc
H.Merijn Brand <h.m.brand@xs4all.nl>
2014-10-03 18:33:02 +0200

    playing some more
    perl6 is not perl5-alike anymore. not even close!
    
     $x =~ s{x}{y}g;
    
    =>
    
     $x ~~ s:g{x} = "y";
    
    yeah, DWIM? NOOOOO!
    
    and their whitespace choices are still counterproductive for me
    and ugly

--- test.pl
+++ test.pl
@@ -1,17 +1,38 @@
 use v6;
 
-class CSV::Field {
+my $test = q{,1,ab,"cd","e"0f","g,h","nl\nz"0i""""3",\r\n};
+my @rslt = ("", "1", "ab", "cd", "e\c0f", "g,h", qq{nl\nz\c0i""3}, "");
+
+sub progress (*@y) {
+    my Str $x;
+    for (@y) {
+#	    s{^(\d+)}   = sprintf "%3d:", $1;
+	s:g{","\s+} = ",\t";
+	s:g{(\w)\(} = "$1 (";
+	$x ~= .Str;
+	}
+    $x.say;
+    } # progress
+
+class CSV:Field {
 
     has Str  $.text		is rw;
     has Bool $.is_quoted	is rw = False;
-    has Bool $.is_binary	is rw = False;
-    has Bool $.is_utf8		is rw = False;
+#   has Bool $.is_binary	is rw = False;
+#   has Bool $.is_utf8		is rw = False;
+    has Bool $.undefined	is rw = True;
 
     enum Type < NA INT NUM STR BOOL >;
 
     method add (Str $chunk) {
-        $.text ~= $chunk;
+        $!text     ~= $chunk;
+        $!undefined = False;
         } # add
+
+    method set_quoted () {
+	$!is_quoted = True;
+        .add("");
+	}
 
     } # CSV::Field
 
@@ -48,23 +69,84 @@ class Text::CSV {
         my     $field;
         my Int $pos   = 0;
 
-        my sub store () {
-            @!fields.push($field);
-            $field = Nil;
-	    }
-
         my sub parse_error (Str $reason, *@args) {
             my $msg = $reason.sprintf(@args);
             die "$msg\n$buffer\n" ~ ' ' x $pos ~ "^\n";
             }
 
+        say $buffer.perl;
+        # A scoping bug in perl6 inhibits the use of $!eol inside the split
+        #for $buffer.split(rx{ $!eol | $!sep | $!quo | $!esc }, :all).map(~*) -> Str $chunk {
+        my     $eol = $!eol // rx{ \r\n | \r | \n };
+        my Str $sep = $!sep;
+        my Str $quo = $!quo;
+        my Str $esc = $!esc;
+        my $f = CSV:Field.new;
+
         @!fields = Nil;
 
-        say $buffer.perl;
-        for split(rx{ $!eol | $!sep | $!quo | $!esc }, $buffer, :all) {
-	    .say;
-            $pos += .length;
+        sub keep {
+	    # Set is_binary
+	    # Set is_utf8
+	    @!fields.push($f);
+	    $f = CSV:Field.new;
+	    } # add
+
+        my @ch = grep { .Str ne "" },
+	    $buffer.split(rx{ $eol | $sep | $quo | $esc }, :all).map(~*);
+
+        for @ch.kv -> $i, Str $chunk {
+
+	    #progress("** $i: '$chunk'\t", $f.perl);
+
+            if $chunk ~~ rx{^ $eol $} {
+		progress("EOL");
+		if $f.is_quoted {	# 1,"2\n3"
+		    $f.add($chunk);
+		    next;
+		    }
+		keep;
+		return @!fields;
+		}
+
+            if $chunk eq $sep {
+		#progress("$i SEP");
+                if $f.is_quoted {	# "1,2"
+                    $f.add($chunk);
+                    next;
+                    }
+                keep;			# 1,2
+                next;
+                }
+
+            if $chunk eq $quo {
+		progress("$i QUO: ", $f.perl);
+                if $f.undefined {
+		    $f.set_quoted;
+		    next;
+		    }
+                if $f.is_quoted {
+                    progress("$i QUO: next = @ch[$i + 1]");
+                    if @ch[$i + 1] ~~ /^$sep/ {
+			keep;
+			next;
+			}
+                    $f.add($chunk);
+                    next;
+                    }
+                keep;
+                next;
+                }
+
+            if $chunk eq $esc {
+		}
+
+            $chunk ne "" and $f.add($chunk);
+            $pos += .chars;
             }
+
+	keep;
+	return @!fields;
         } # parse
 
     method getline () {
@@ -77,49 +159,28 @@ sub MAIN () {
     my $csv_parser = Text::CSV.new;
 
     say $csv_parser.perl;
-    $csv_parser.parse(q{ab,cde,,1,"q",});
-        #say $csv_parser.getline().perl;
+    progress (.perl) for $csv_parser.parse($test);
     }
 
-#
-#{   local $/ = $eol;
-#    my @field;
-#    my $field;
-#    # split - and keep - on special sequences
-#    while (my @chunks = split m{ ( $eol | $sep | $quo | $esc ) } => <$fh>) {
-#        foreach my $chunk (@chunk) {
-#            if ($chunk eq $eol) {
-#                if ($field.quoted) {
-#                    $field.add ($chunk)
-#                    next;
-#                    }
-#                $field.defined or push @fields, $field;
-#                return @fields;
-#                }
-#            if ($chunk eq $sep) {
-#                if ($field.quoted) {
-#                    $field.add ($chunk)
-#                    next;
-#                    }
-#                push @fields, $field;
-#                undef $field;
-#                next;
-#                }
-#            if ($chunk eq $quo) {
-#                if (!defined $field) {
-#                    $field = "";
-#                    $field.set_quoted;
-#                    next;
-#                    }
-#                # moeilijke code als omschreven in flow
-#                # deal with esc eq sep
-#                next;
-#                }
-#            if ($chunk eq $esc) {
-#                ...
-#                next;
-#                }
-#            $field.add ($chunk);
-#            }
-#        }
-#    }
+=begin plan
+
+            if ($chunk eq $quo) {
+                if (!defined $field) {
+                    $field = "";
+                    $field.set_quoted;
+                    next;
+                    }
+                # moeilijke code als omschreven in flow
+                # deal with esc eq sep
+                next;
+                }
+            if ($chunk eq $esc) {
+                ...
+                next;
+                }
+            $field.add ($chunk);
+            }
+        }
+    }
+
+=end plan
