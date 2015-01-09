3e36bfba7a0e8d4d1a80552a6d4d198bc131dc88
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2015-01-09 09:02:29 +0100

    read hello.csv before running tests

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# make FS cache hello.csv to make a more honest test case
+     perl  csv-test-xs.pl < /tmp/hello.csv > /dev/null 2>&1
+
 time perl  csv-easy-xs.pl < /tmp/hello.csv
 time perl  csv-easy-pp.pl < /tmp/hello.csv
 time perl  csv-test-xs.pl < /tmp/hello.csv
