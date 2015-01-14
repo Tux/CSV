9eb25681f85b7e8a2cd3a4b7d18e4c808dd2d77b
H.Merijn Brand <h.m.brand@xs4all.nl>
2015-01-14 11:59:31 +0100

    Newest timings

--- test.sh
+++ test.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 # make FS cache hello.csv to make a more honest test case
-     perl  csv-test-xs.pl < /tmp/hello.csv > /dev/null 2>&1
+cat      /tmp/hello.csv | perl  csv-easy-xs.pl >/dev/null 2>&1
+head -30 /tmp/hello.csv | perl  csv-easy-pp.pl >/dev/null 2>&1
+head -30 /tmp/hello.csv | perl  csv-test-xs.pl >/dev/null 2>&1
+head -30 /tmp/hello.csv | perl  csv-test-pp.pl >/dev/null 2>&1
+head -30 /tmp/hello.csv | perl  csv-pegex.pl   >/dev/null 2>&1
+head -30 /tmp/hello.csv | perl6 csv.pl         >/dev/null 2>&1
+head -30 /tmp/hello.csv | perl6 csv_gram.pl    >/dev/null 2>&1
+head -30 /tmp/hello.csv | perl6 test.pl        >/dev/null 2>&1
+head -30 /tmp/hello.csv | perl6 test-t.pl      >/dev/null 2>&1
 
 time perl  csv-easy-xs.pl < /tmp/hello.csv
 time perl  csv-easy-pp.pl < /tmp/hello.csv
