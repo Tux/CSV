3e97636bda99f77e81f366503eca7c7656d6e657
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2015-01-14 14:46:56 +0100

    No debugging lines gains me 30%!!

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 export PERL6_VERBOSE=0
 
+perl -pe's/^ ?(?=\s*.opt_v)/#/' test-t.pl > test-x.pl
+
 # make FS cache hello.csv to make a more honest test case
 cat      /tmp/hello.csv | perl  csv-easy-xs.pl >/dev/null 2>&1
 head -30 /tmp/hello.csv | perl  csv-easy-pp.pl >/dev/null 2>&1
@@ -11,7 +13,7 @@ head -30 /tmp/hello.csv | perl  csv-pegex.pl   >/dev/null 2>&1
 head -30 /tmp/hello.csv | perl6 csv.pl         >/dev/null 2>&1
 head -30 /tmp/hello.csv | perl6 csv_gram.pl    >/dev/null 2>&1
 head -30 /tmp/hello.csv | perl6 test.pl        >/dev/null 2>&1
-head -30 /tmp/hello.csv | perl6 test-t.pl      >/dev/null 2>&1
+head -30 /tmp/hello.csv | perl6 test-x.pl      >/dev/null 2>&1
 
 time perl  csv-easy-xs.pl < /tmp/hello.csv
 time perl  csv-easy-pp.pl < /tmp/hello.csv
@@ -21,5 +23,5 @@ time perl  csv-pegex.pl   < /tmp/hello.csv
 time perl6 csv.pl         < /tmp/hello.csv
 time perl6 csv_gram.pl    < /tmp/hello.csv
 time perl6 test.pl        < /tmp/hello.csv
-time perl6 test-t.pl      < /tmp/hello.csv
+time perl6 test-x.pl      < /tmp/hello.csv
 
