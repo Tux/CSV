9486cddaf650ed4589a81445b0bb01da64b51fbd
H.Merijn Brand <h.m.brand@xs4all.nl>
2015-01-16 13:40:48 +0100

    Two ways to compare timings
    I have no idea where the difference for test.pl comes from
    
                             time.pl  test.sh
    csv-easy-xs      50000     0.020    0.022
    csv-easy-pp      50000     0.019    0.020
    csv-test-xs      50000     0.038    0.047
    csv-test-pp      50000     0.541    0.519
    csv-pegex        50000     1.349    1.347
    csv              50000     7.795    7.934
    csv_gram    ***  60000    13.739   13.760
    test             50000    39.628   52.963
    test-x           50000    38.162   38.751

--- test.sh
+++ test.sh
@@ -4,27 +4,20 @@ export PERL6_VERBOSE=0
 
 perl -pe's/^ ?(?=\s*.opt_v)/#/' test-t.pl > test-x.pl
 
-# make FS cache hello.csv to make a more honest test case
-cat      /tmp/hello.csv | perl  csv-easy-xs.pl >/dev/null 2>&1
-head -30 /tmp/hello.csv | perl  csv-easy-pp.pl >/dev/null 2>&1
-head -30 /tmp/hello.csv | perl  csv-test-xs.pl >/dev/null 2>&1
-head -30 /tmp/hello.csv | perl  csv-test-pp.pl >/dev/null 2>&1
-head -30 /tmp/hello.csv | perl  csv-pegex.pl   >/dev/null 2>&1
-head -30 /tmp/hello.csv | perl6 csv.pl         >/dev/null 2>&1
-head -30 /tmp/hello.csv | perl6 csv_gram.pl    >/dev/null 2>&1
-head -30 /tmp/hello.csv | perl6 test.pl        >/dev/null 2>&1
-head -30 /tmp/hello.csv | perl6 test-x.pl      >/dev/null 2>&1
-
+(
 for t in csv-easy-xs csv-easy-pp csv-test-xs csv-test-pp csv-pegex ; do
     echo
     echo "******* $t"
+    head -30 /tmp/hello.csv | perl  $t.pl >/dev/null 2>&1
     time perl  $t.pl < /tmp/hello.csv
     done
 
 for t in csv csv_gram test test-x ; do
     echo
     echo "******* $t"
+    head -30 /tmp/hello.csv | perl6 $t.pl >/dev/null 2>&1
     time perl6 $t.pl < /tmp/hello.csv
     done
+) 2>&1 | perl -ne'm/^(user|sys|Array|\$)/ or print'
 
 rm -f test-x.pl
---
+++ time.pl
@@ -0,0 +1,53 @@
+#!/pro/bin/perl
+
+use 5.18.2;
+use warnings;
+
+use Time::HiRes qw( gettimeofday tv_interval );
+
+unlink "test-x.pl"; END { unlink "test-x.pl" }
+open my $th, "<", "test-t.pl" or die "test-t.pl: $!\n";
+open my $xh, ">", "test-x.pl" or die "test-x.pl: $!\n";
+while (<$th>) {
+    s/^ ?(?=\s*.opt_v)/#/;
+    print $xh $_;
+    }
+close $th;
+close $xh;
+
+open  my $fh, "<", "/tmp/hello.csv";
+1 while <$fh>;
+close    $fh;
+
+my %perl = (
+    5 => "perl",
+    6 => "perl6",
+    );
+my @test = (
+    [ 5, "csv-easy-xs" ],
+    [ 5, "csv-easy-pp" ],
+    [ 5, "csv-test-xs" ],
+    [ 5, "csv-test-pp" ],
+    [ 5, "csv-pegex"   ],
+    [ 6, "csv"         ],
+    [ 6, "csv_gram"    ],
+    [ 6, "test"        ],
+    [ 6, "test-x"      ],
+    );
+
+for (@test) {
+    open my $ph, "|-", "$perl{$_->[0]} $_->[1].pl 2>&1 >/dev/null";
+    print   $ph "\n";
+    close   $ph;
+
+    printf "%-11s ", $_->[1];
+    my $t0 = [ gettimeofday ];
+    open my $th, "-|", "$perl{$_->[0]} $_->[1].pl 2>&1 </tmp/hello.csv";
+    my $i = 0;
+    while (<$th>) {
+        m/^(\d+)$/ and $i = $1;
+        }
+    my $elapsed = tv_interval ($t0);
+    close $th;
+    printf "%s %6d %9.3f\n", $i eq 50000 ? "   " : "***", $i, $elapsed;
+    }
