945d5e5d05f686fa3d8d1d2e826164c63a772eea
H.Merijn Brand <h.m.brand@xs4all.nl>
2015-02-06 09:37:49 +0100

    Show run-time (subtract average start-time) too

--- time.pl
+++ time.pl
@@ -19,10 +19,6 @@ open  my $fh, "<", "/tmp/hello.csv";
 1 while <$fh>;
 close    $fh;
 
-my %perl = (
-    5 => "perl",
-    6 => "perl6",
-    );
 my @test = (
     [ 5, "csv-easy-xs" ],
     [ 5, "csv-easy-pp" ],
@@ -35,20 +31,37 @@ my @test = (
     [ 6, "test"        ],
     [ 6, "test-x"      ],
     );
+my %perl = (
+    5 => "perl",
+    6 => "perl6",
+    );
+my %start;
+foreach my $v (keys %perl) {
+    my $t = 0;
+    for (1 .. 5) {
+        my $t0 = [ gettimeofday ];
+        open my $th, "-|", "$perl{$v} -e 1 2>&1 >/dev/null";
+        close $th;
+        $t += tv_interval ($t0);
+        }
+    $start{$v} = $t / 5;
+    }
 
 for (@test) {
-    open my $ph, "|-", "$perl{$_->[0]} $_->[1].pl 2>&1 >/dev/null";
+    my ($v, $script) = @$_;
+    open my $ph, "|-", "$perl{$v} $script.pl 2>&1 >/dev/null";
     print   $ph "\n";
     close   $ph;
 
     printf "%-11s ", $_->[1];
     my $t0 = [ gettimeofday ];
-    open my $th, "-|", "$perl{$_->[0]} $_->[1].pl 2>&1 </tmp/hello.csv";
+    open my $th, "-|", "$perl{$v} $script.pl 2>&1 </tmp/hello.csv";
     my $i = 0;
     while (<$th>) {
         m/^(\d+)$/ and $i = $1;
         }
     my $elapsed = tv_interval ($t0);
     close $th;
-    printf "%s %6d %9.3f\n", $i eq 50000 ? "   " : "***", $i, $elapsed;
+    printf "%s %6d %9.3f %9.3f\n", $i eq 50000 ? "   " : "***", $i,
+        $elapsed, $elapsed - $start{$v};
     }
