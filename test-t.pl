33bcec2711b88d04d7c7643459981c61b9445952
H.Merijn Brand <h.m.brand@xs4all.nl>
2015-04-07 09:13:45 +0200

    And create a new test file

---
+++ test-t.pl
@@ -0,0 +1,27 @@
+#!perl6
+
+use v6;
+use Slang::Tuxic;
+use Text::CSV;
+
+sub MAIN (:$getline, :$getline_all) {
+
+    my $csv = Text::CSV.new;
+
+    my Int $sum = 0;
+    if ($getline_all) { # slowest
+        $sum = [+] $csv.getline_all ($*IN)».map(*.elems);
+        }
+    elsif ($getline) {  # middle, but safe
+        while ($csv.getline ($*IN)) {
+            $sum += $csv.fields.elems;
+            }
+        }
+    else {              # fastest, but unsafe
+        for lines () :eager {
+            $csv.parse ($_);
+            $sum += $csv.fields.elems;
+            }
+        }
+    $sum.say;
+    }
