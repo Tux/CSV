c6874979db3271d1e97852719ca1641995582ff8
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2016-01-20 10:26:08 +0100

    Directly update "Other languages" page in CSV6 talk

--- time.pl
+++ time.pl
@@ -40,7 +40,7 @@ my %lang = (
      6 => [ ".pl",  "perl6",   "-Ilib" ],
      7 => [ ".lua", "lua"              ],
      8 => [ ".go",  "go",      "run"   ],
-     9 => [ "",     "c"                ],
+     9 => [ "",     "C"                ],
     10 => [ "",     "java7",   "-cp csv-java7.jar:opencsv-2.3.jar csvJava" ],
     11 => [ "",     "java8",   "-cp csv-java8.jar:opencsv-2.3.jar csvJava" ],
     12 => [ "",     "java9",   "-cp csv-java9.jar:opencsv-2.3.jar csvJava" ],
@@ -80,7 +80,7 @@ sub runfrom {
     my ($v, $script, $file) = @_;
     my ($ext, $exe, @arg) = @{$lang{$v}};
 
-    $exe eq "c" and $exe = "";
+    $exe eq "C" and $exe = "";
     my $run = join " " => $exe, @arg;
 
     $opt_v > 4 and say "$v / $ext / $exe\t/ $run";
@@ -99,6 +99,7 @@ sub runfrom {
 my $pat = shift // ".";
 
 my $run_speed = 0;
+my @time;
 my @irc;
 for (@test) {
     my ($v, $irc, $script) = @$_;
@@ -128,6 +129,7 @@ for (@test) {
     my $s = sprintf "%s %6d %9.3f %9.3f", $i eq 50000 ? "   " : "***", $i,
         $run, $run - $start;
     say $s;
+    push @time, [ $script, $s_script, $i, $run, $start, $exe ];
 
     if ($script eq "test-t" and open my $fh, ">>", "../Talks/CSV6/speed.log") {
         my @d = localtime;
@@ -138,10 +140,77 @@ for (@test) {
         }
 
     $opt_i and next;
-    $irc and push @irc, "$s_script\t$s";
+    $irc and push @irc, $time[-1];
     }
 
-say for @irc;
+printf "%s %9.3f\n", $_->[1], $_->[3] for @irc;
+
+if (!$opt_i and open my $fh, ">", "../Talks/CSV6/speed5.html") {
+    print $fh <<'EOH';
+<?xml version="1.0" encoding="iso-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <title>Parsing CSV</title>
+  <meta name="Generator"     content="elvis 2.2" />
+  <meta name="Author"        content="H.Merijn Brand" />
+  <meta name="Description"   content="Parsing CSV in perl" />
+
+  <link rel="SHORTCUT ICON"  href="images/csv.ico" />
+
+  <link rel="stylesheet" type="text/css"  href="csv.css" />
+  <link rel="next"       type="text/html" href="attributes.html" />
+  <style type="text/css">
+    td { text-align: left; }
+    thead > tr > td { font-weight: bold; background: #c0c0c0; }
+    </style>
+  </head>
+<body>
+<table>
+  <tr class="boxed">
+    <td class="boxed">
+      <h1><a class="navF" href="attributes.html">Other languages</a></h1>
+      <table>
+	<thead>
+	  <tr><td>language</td><td>script</td><td>check</td><td>count</td><td>time</td><td>runtime</td></tr>
+	  </thead>
+	<tbody>
+EOH
+    foreach my $t (sort { $a->[3] <=> $b->[3] } @time) {
+        my ($script, $s_script, $i, $run, $start, $exe) = @$t;
+        my ($s_run, $s_run2) = map { sprintf "%.3f", $_ } $run, $run - $start;
+        $s_run2 =~ m/^-/ and $s_run2 = "0.000";
+        $exe =~ s/perl$/perl5/;
+        say $fh
+            qq{\t  <tr@{[$exe =~ m/^perl/ ? qq{ class="$exe"} : ""]}>},
+                qq{<td>$exe</td>},
+                qq{<td>$script</td>},
+                qq{<td>@{[$i == 50000 ? "&nbsp;" : "***"]}</td>},
+                qq{<td class="@{[$i == 50000 ? 'fixed'  : 'broken']}">$i</td>},
+                qq{<td class="time">$s_run</td>},
+                qq{<td class="time">$s_run2</td>},
+                qq{</tr>};
+        }
+    my @d = localtime;
+    print $fh <<"EOF";
+	  </tbody>
+	</table>
+      <ul>
+	<li><strong>count</strong> should be 50000</li>
+	<li><strong>time</strong> is the time taken to parse 10000 CSV lines with 5 fields each</li>
+	<li><strong>runtime</strong> is time minus time taken to run script from an empty stream</li>
+	</ul>
+      <p>See also <a href="https://bitbucket.org/ewanhiggs/csv-game">the CSV game</a>.</p>
+      </td>
+    </tr>
+  </table>
+
+<p class="update">last update: @{[join "-" => map { sprintf "%02d", $_ } $d[3], $d[4] + 1, $d[5] + 1900]}</p>
+</body>
+</html>
+EOF
+    close $fh;
+    }
 
 if ($run_speed) {
     chdir "../Talks/CSV6";
