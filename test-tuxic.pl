abf68c1e3b47bed2314d72f950adf1fa655c3b3e
Elizabeth Mattijsen <liz@dijkmat.nl>
2017-04-30 15:36:46 +0200

    Move test-t.pl -> test-tuxic.pl
    Testing Text::CSV as a "normal" user, shouldn't need to incur the grammar
    rewriting overhead of Slang::Tuxic.  So to get a better idea of the
    performance of Text::CSV for "normal" users, it feels like a good idea
    to have test-t.pl be in "generic" Perl 6.  To allow testing *with*
    Slang::Tuxic, we now have the test-tuxic.pl file (which contains the
    original version of test-t.pl).

--- test-tuxic.pl
+++ test-tuxic.pl
@@ -1,6 +1,7 @@
 #!perl6
 
 use v6;
+use Slang::Tuxic;
 use Text::CSV;
 
 sub MAIN (:$getline, :$getline_all) {
@@ -8,17 +9,17 @@ sub MAIN (:$getline, :$getline_all) {
     my $csv = Text::CSV.new;
 
     my Int $sum = 0;
-    if $getline_all { # slowest
-        $sum = [+] $csv.getline_all($*IN)».map(*.elems);
+    if ($getline_all) { # slowest
+        $sum = [+] $csv.getline_all ($*IN)».map(*.elems);
         }
-    elsif $getline {  # middle, but safe
-        while $csv.getline($*IN) {
+    elsif ($getline) {  # middle, but safe
+        while ($csv.getline ($*IN)) {
             $sum += $csv.fields.elems;
             }
         }
     else {              # fastest, but unsafe
-        for lines() {
-            $csv.parse($_);
+        for lines () {
+            $csv.parse ($_);
             $sum += $csv.fields.elems;
             }
         }
