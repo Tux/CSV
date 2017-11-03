9708c7980f6bedb7120efc34408333f86c90f161
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2017-11-03 08:22:04 +0100

    Add java10 to timings

--- time.pl
+++ time.pl
@@ -50,10 +50,12 @@ my %lang = (
     15 => [ "",     "C++"              ],
     16 => [ "",     "java8",   "-cp csv-pi-easy-pp.jar Main /tmp/hello.csv" ],
     17 => [ "",     "Rust",    "/tmp/hello.csv" ],
+    18 => [ "",     "java10",  "-cp csv-java10.jar:opencsv-2.3.jar csvJava" ],
     );
 my @test = (
     # lang irc script
     [  5, 0, "csv-easy-xs"      , "Text::CSV::Easy_XS" ],
+    [  5, 0, "csv-easy-xs-20"   , "Text::CSV::Easy_XS" ],
     [  5, 0, "csv-easy-pp"      , "Text::CSV::Easy_PP" ],
     [  5, 0, "csv-xsbc"         , "Text::CSV_XS" ],
     [  5, 0, "csv-test-xs"      , "Text::CSV_XS" ],
@@ -72,17 +74,19 @@ my @test = (
     [  6, 1, "test-t-20"        , "Text::CSV"     , "--race"    ],
     [  6, 1, "csv-parser"       , "CSV::Parser"  ],
     [  9, 0, "csv-c"           ],
+    [  9, 0, "csv-c-20"        ],
     [ 15, 0, "csv-cc"          ],
     [  7, 0, "csv-lua"         ],
     [  2, 0, "csv-python2"     ],
     [  3, 0, "csv-python3"     ],
     [  4, 0, "csv-php"         ],
+    [ 18, 0, "csv-java10"      ],
+    [ 12, 0, "csv-java9"       ],
     [ 11, 0, "csv-java8"       ],
     [ 14, 0, "csv-java6"       ],
     [ 10, 0, "csv-java7"       ],
     [  8, 0, "csv-go"          ],
     [ 13, 0, "csv-R"           ],
-    [ 12, 0, "csv-java9"       ],
     [ 16, 0, "csv-easy-pp-pi", "Text::CSV::Easy_PP, Perlito" ],
     [ 17, 0, "csv-rust-csvrdr" ],
     [ 17, 0, "csv-rust-libcsv" ],
@@ -152,9 +156,9 @@ for (@test) {
         $ec *= $fc;
         }
 
-		    runfrom ($v, $rs, "empty.csv");
-		    runfrom ($v, $rs, "empty.csv");
-    my ($start)   = runfrom ($v, $rs, "empty.csv");
+    my $start = min runfrom ($v, $rs, "empty.csv"),
+		    runfrom ($v, $rs, "empty.csv"),
+                    runfrom ($v, $rs, "empty.csv");
     my ($run, $i) = runfrom ($v, $rs, $fn, @args);
 
     my $s = sprintf "%s %7d %9.3f %9.3f", $i eq $ec ? "   " : "***", $i,
@@ -196,6 +200,7 @@ if (!$opt_i and open my $fh, ">", "../Talks/CSV6/speed5.html") {
   <style type="text/css">
     td { text-align: left; }
     thead > tr > td { font-weight: bold; background: #c0c0c0; }
+    .fixed, .broken { text-align: right; }
     </style>
   </head>
 <body>
@@ -205,46 +210,76 @@ if (!$opt_i and open my $fh, ">", "../Talks/CSV6/speed5.html") {
       <h1><a class="navF" href="attributes.html">Other languages</a></h1>
       <table>
 	<thead>
-	  <tr><td>language</td><td>script</td><td>modules</td><td>count</td><td>time</td><td>runtime</td></tr>
+	  <tr><td>language</td><td>script</td><td>modules</td><td>count</td><td>norm</td><td>time</td><td>runtime</td></tr>
 	  </thead>
 	<tbody>
 EOH
-    foreach my $t (sort { $a->[3] <=> $b->[3] } @time) {
-	my ($script, $s_script, $i, $run, $start, $exe, $modules) = @$t;
+    my @r = map {
+	my ($script, $s_script, $i, $run, $start, $exe, $modules, @args) = @$_;
 	$i == 0 and $i = "FAIL";
-	my ($s_run, $s_run2) = map { $i eq "FAIL" ? qq{<span class="broken">-</span>} : sprintf "%.3f", $_ } $run, $run - $start;
+	my $N = 50000;
+	my $n = $s_script =~ m/-20/ ? $N * 20 : $N;
+	my $runn = ($run || 0) * $N / $n;
+	my ($s_run, $s_run2, $s_runn) = map { $i eq "FAIL"
+            ? qq{<span class="broken">-</span>}
+            : sprintf "%.3f", $_
+            } $run, $run - $start, $runn;
 	$s_run2 =~ m/^-/ and $s_run2 = "0.000";
-	$t->[7] = +($s_run2 || 0);
-	}
-    foreach my $t (sort { $a->[3] <=> $b->[3]
-                       || $a->[7] <=> $b->[7]
-                        } @time) {
-	my ($script, $s_script, $i, $run, $start, $exe, $modules) = @$t;
+	{   script	=> $script,
+	    s_script	=> $s_script =~ s/\s+$//r,
+	    i		=> $i,
+	    n           => $n,
+	    run		=> $run,
+	    run2	=> +($s_run2 || 0),
+	    runn	=> $runn,
+	    s_run	=> $s_run,
+	    s_run2	=> $s_run2,
+	    s_runn	=> $s_runn,
+	    start	=> $start,
+	    exe		=> $exe,
+	    modules	=> $modules,
+	    args	=> "@args",
+	    };
+	} @time;
+#use DP;DDumper \@r;
+    foreach my $t (sort { $a->{runn} <=> $b->{runn}
+                       || $a->{run}  <=> $b->{run}
+                       || $a->{run2} <=> $b->{run2}
+                        } @r) {
+	my $i = $t->{i};
 	$i == 0 and $i = "FAIL";
-	my ($s_run, $s_run2) = map { $i eq "FAIL" ? qq{<span class="broken">-</span>} : sprintf "%.3f", $_ } $run, $run - $start;
-	$s_run2 =~ m/^-/ and $s_run2 = "0.000";
-	$exe =~ s/perl$/perl5/;
-	my $class = $script =~ m/-pi\b/ ? "perlito" : $exe =~ m/^perl/ ? $exe : "";
+	$t->{exe} =~ s/perl$/perl5/;
+	my $class = $t->{script} =~ m/-pi\b/ ? "perlito" : $t->{exe} =~ m/^perl/ ? $t->{exe} : "";
+	my $scrpt = join " " => grep m/\S/ => $t->{s_script}, $t->{args};
+	#DDumper { t => $t, class => $class, script => $scrpt };
 	say $fh
 	    qq{\t  <tr@{[$class ? qq{ class="$class"} : ""]}>},
-		qq{<td>$exe</td>},
-		qq{<td>$script</td>},
-		qq{<td>$modules</td>},
-		qq{<td class="@{[$i eq 50000 ? 'fixed'  : 'broken']}">$i</td>},
-		qq{<td class="time">$s_run</td>},
-		qq{<td class="time">$s_run2</td>},
+		qq{<td>$t->{exe}</td>},
+		qq{<td>$scrpt</td>},
+		qq{<td>$t->{modules}</td>},
+		qq{<td class="@{[$i eq $t->{n} ? 'fixed'  : 'broken']}">$i</td>},
+		qq{<td class="time">$t->{s_runn}</td>},
+		qq{<td class="time">$t->{s_run}</td>},
+		qq{<td class="time">$t->{s_run2}</td>},
 		qq{</tr>};
 	}
     my @d = localtime;
     print $fh <<"EOF";
 	  </tbody>
 	</table>
-      <ul>
-	<li><strong>count</strong> should be 50000</li>
-	<li><strong>time</strong> is the time taken to parse 10000 CSV lines with 5 fields each</li>
-	<li><strong>runtime</strong> is time minus time taken to run script from an empty stream</li>
-	</ul>
-      <p>See also <a href="https://bitbucket.org/ewanhiggs/csv-game">the CSV game</a>.</p>
+      </td>
+    </tr>
+  <tr class="boxed">
+    <td class="boxed">
+      <table>
+        <tr><td colspan="2">The default CSV file consists of 10000 lines with 5 fields each.</td></tr>
+	<tr><td><strong>count</strong></td><td>the number of CSV fields counted</td></tr>
+	<tr><td><strong>norm</strong></td><td>the time taken to parse normalized to parsing 10000 lines</td></tr>
+	<tr><td><strong>time</strong></td><td>the time taken to parse</td></tr>
+	<tr><td><strong>runtime</strong></td><td>time minus time taken to run script from an empty stream</td></tr>
+	</table>
+      <br />
+      See also <a href="https://bitbucket.org/ewanhiggs/csv-game">the CSV game</a>.
       </td>
     </tr>
   </table>
