52a9587d555ec3c49c417cfd7fbcb4bac739dbb2
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2020-10-21 17:30:54 +0200

    I *really* don't like the new version reporting. It is plain stupid

--- time-twice.pl
+++ time-twice.pl
@@ -11,9 +11,7 @@ GetOptions (
     "s|silent!" => \my $opt_s,
     ) or die "usage: $0 [--silent] [options to time.pl]\n";
 
-my $v;
-my %t;
-my %seen;
+my (@v, %t, %seen);
 foreach my $i (1, 2) {
     print "\r";
     open my $th, "-|", "time.pl", @ARGV;
@@ -24,8 +22,16 @@ foreach my $i (1, 2) {
             next;
             }
         # print;
-        if (m/^This is/) {
-            $v //= $_;
+        if (m/^(?:This is|Welcome to)\s.*\s(v\d+[-.\w]+?)\.?$/) {
+            $v[0] //= "Rakudo $1";
+            next;
+            }
+        if (m/^Implementing\s.*(v\d\S+?)\.?$/) {
+            $v[1] //= " ($1)";
+            next;
+            }
+        if (m/^Built on (\w+)\s+version\s+(\S+?)\.?$/) {
+            $v[2] //= " on $1 $2";
             next;
             }
         my ($s, $t) = m/^(.+?)\s+([0-9][.0-9]+)$/ or next;
@@ -34,7 +40,7 @@ foreach my $i (1, 2) {
     }
 
 say "";
-print $v =~ s/^This is //r =~ s/ built on / - /r;
+say join "" => grep { length } @v;
 foreach my $t (sort { $t{$a}[0] <=> $t{$b}[0] } keys %t) {
     my @t = sort { $a <=> $b } @{$t{$t}};
     printf "%-18s %s\n", $t, join " - " => map { sprintf "%6.3f", $_ } @t[0,-1];
